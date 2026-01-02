cfb9653b7061169f524e3df2c02f9f5dfc817c9e
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2026-01-02 20:41:33 +0530

    add p-code error messages

--- logic/modules/formValidator.js
+++ logic/modules/formValidator.js
@@ -8,6 +8,25 @@ function handleCountryField(value) {
   }
 }
 
+function handleMessage(field) {
+  if (field.id == "p-code") {
+    const value = field.value;
+    const [char, numbers] = value.split('-');
+
+    if (!char || !numbers) {
+      return "Postal Code must contain a dash separating character and digits."
+    }
+
+    if (!/^[a-zA-Z]$/.test(char)) {
+      return "Postal Code must have a single letter before '-'.";
+    }
+    
+    if (!/^\d$/.test(numbers)) {
+      return "Postal Code must have exactly 4 digits after '-'.";
+    }
+  }
+}
+
 // Validate a single field
 function validateField(event) {
   const target = event.target || event;
@@ -16,6 +35,8 @@ function validateField(event) {
   if (id === "country") {
     handleCountryField(value);
   }
+
+  console.log( handleMessage(target) );
   
   const errorMessage = getErrorMessage(validity);
   showMessage(target, errorMessage);
